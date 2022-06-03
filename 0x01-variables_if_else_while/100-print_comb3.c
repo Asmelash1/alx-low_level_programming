2620cc9713a157000cc979433997f58733cbd73b
Asmelash1 <abrihamasmelash@gmail.com>
2022-06-03 15:05:24 -0700

    two digits up to 89

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of two digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * main - main block
+ * Description: Print all possible combinations of two digits.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
  * Return: 0
  */
 int main(void)
 {
-int n1 = 48;
-int n2;
-int com = 44;
-while (n1 <= 57)
-{
-n2 = n1 + 1;
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 56 || n2 != 57)
-{
-putchar(com);
-putchar(32);
+	int i, j, k;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		j = i % 10; /* singles digit */
+		k = i / 10; /* doubles digit */
+		if (k < j)
+
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+			if (i < 89)
+
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
 }
-n2 += 1;
-}
 
-n1 += 1;
-
-}
-
-putchar('\n');
-
-return (0);
-
-}
